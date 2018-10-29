4903e5353fdfa8512ceba117a17f384989e2c5aa
Luciano Rossi <lukio@gcoop.coop>
2018-10-29 18:32:45 -0300

    fix #6 python3 migration. move metaclass PoolMeta inside the class.

--- __init__.py
+++ __init__.py
@@ -2,25 +2,24 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
-from .subdiario import *
+from . import invoice
 
 
 def register():
     Pool.register(
-        Invoice,
-        SubdiarioPurchaseStart,
-        SubdiarioSaleStart,
+        invoice.Invoice,
+        invoice.SubdiarioPurchaseStart,
+        invoice.SubdiarioSaleStart,
         module='subdiario', type_='model')
     Pool.register(
-        SubdiarioPurchase,
-        SubdiarioSale,
-        SubdiarioSaleType,
-        SubdiarioSaleSubdivision,
+        invoice.SubdiarioPurchase,
+        invoice.SubdiarioSale,
+        invoice.SubdiarioSaleType,
+        invoice.SubdiarioSaleSubdivision,
         module='subdiario', type_='wizard')
     Pool.register(
-        SubdiarioPurchaseReport,
-        SubdiarioSaleReport,
-        SubdiarioSaleTypeReport,
-        SubdiarioSaleSubdivisionReport,
+        invoice.SubdiarioPurchaseReport,
+        invoice.SubdiarioSaleReport,
+        invoice.SubdiarioSaleTypeReport,
+        invoice.SubdiarioSaleSubdivisionReport,
         module='subdiario', type_='report')
