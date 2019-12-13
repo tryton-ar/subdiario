91cdb03e66544516a055bd2ed2d4ab01c3779a1b
Luciano Rossi <lukio@gcoop.coop>
2019-12-13 19:00:43 -0300

    fixes #7 Facturacion en moneda secundaria. Mejora salida del reporte de venta.
    Utiliza los campos currency_rate, party_tax_identifier y party_iva_condition de
    account_invoice_ar.
    
    De esa manera, logramos que el subdiario obtenga los datos del
    comprobante de la factura.

--- invoice.py
+++ invoice.py
@@ -12,6 +12,8 @@ from trytond.report import Report
 from decimal import Decimal
 from subdiario import Subdiario
 
+_ZERO = Decimal('0.0')
+
 
 __all__ = ['Invoice', 'SubdiarioPurchaseStart', 'SubdiarioSaleStart',
            'SubdiarioPurchase', 'SubdiarioSale', 'SubdiarioPurchaseReport',
@@ -209,7 +211,8 @@ class SubdiarioSaleStart(ModelView):
     from_date = fields.Date('From Date', required=True)
     to_date = fields.Date('To Date', required=True)
     company = fields.Many2One('company.company', 'Company', required=True)
-    pos = fields.Many2One('account.pos', 'Point of Sale', required=True)
+    pos = fields.Many2Many('account.pos', None, None, 'Point of Sale',
+        required=True, help="Por defecto puntos de venta electronicos")
 
     @staticmethod
     def default_from_date():
@@ -229,15 +232,13 @@ class SubdiarioSaleStart(ModelView):
     @staticmethod
     def default_pos():
         Pos = Pool().get('account.pos')
+        company_id = Transaction().context.get('company', -1)
         pos = Pos.search([
             ('pos_type', '=', 'electronic'),
-            ('pyafipws_electronic_invoice_service', '=', 'wsfe'),
+            ('pos_do_not_report', '=', False),
+            ('company', '=', company_id),
         ])
-        if pos != []:
-            pos = pos[0].id
-        else:
-            pos = None
-        return pos
+        return [p.id for p in pos]
 
 
 class SubdiarioSale(Wizard):
@@ -255,7 +256,7 @@ class SubdiarioSale(Wizard):
             'company': self.start.company.id,
             'from_date': self.start.from_date,
             'to_date': self.start.to_date,
-            'pos': self.start.pos.id,
+            'pos': [p.id for p in self.start.pos],
             }
         return action, data
 
@@ -271,8 +272,13 @@ class SubdiarioSaleReport(Report, Subdiario):
         Tax = pool.get('account.tax')
         Company = pool.get('company.company')
         Pos = pool.get('account.pos')
-        total_amount = Decimal('0')
-        total_untaxed_amount = Decimal('0')
+        total_amount = _ZERO
+        total_untaxed_amount = _ZERO
+        totales_amount = _ZERO
+        totales_untaxed_amount = _ZERO
+        totales_iva105 = _ZERO
+        totales_iva21 = _ZERO
+        totales_iva27 = _ZERO
 
         invoices = Invoice.search([
             ('state', 'in', ['posted', 'paid']),
@@ -280,16 +286,26 @@ class SubdiarioSaleReport(Report, Subdiario):
             ('move.date', '>=', data['from_date']),
             ('move.date', '<=', data['to_date']),
             ('company', '=', data['company']),
-            ('pos', '=', data['pos']),
-        ], order=[('invoice_date', 'ASC')])
+            ('pos', 'in', data['pos']),
+            ], order=[
+            ('pos', 'ASC'),
+            ('invoice_date', 'ASC'),
+            ('invoice_type', 'ASC'),
+            ('number', 'ASC'),
+            ])
 
         company = Company(data['company'])
-        pos = Pos(data['pos'])
 
         for invoice in invoices:
             total_amount = invoice.total_amount + total_amount
             total_untaxed_amount = (invoice.untaxed_amount +
                 total_untaxed_amount)
+        for invoice in invoices:
+            totales_amount += cls.get_amount(invoice, 'total_amount')
+            totales_untaxed_amount += cls.get_amount(invoice, 'untaxed_amount')
+            totales_iva21 += cls.get_iva(invoice, '0.21')
+            totales_iva105 += cls.get_iva(invoice, '0.105')
+            totales_iva27 += cls.get_iva(invoice, '0.27')
 
         taxes = Tax.search([
             ('group.kind', 'in', ['sale', 'both']),
@@ -314,18 +330,23 @@ class SubdiarioSaleReport(Report, Subdiario):
         report_context['company'] = company
         report_context['from_date'] = data['from_date']
         report_context['to_date'] = data['to_date']
-        report_context['pos'] = pos
         report_context['invoices'] = invoices
-        report_context['total_untaxed_amount'] = total_untaxed_amount
-        report_context['total_amount'] = total_amount
-        report_context['format_tipo_comprobante'] = cls.format_tipo_comprobante
+        report_context['format_ci'] = cls.format_ci
         report_context['get_iva'] = cls.get_iva
+        report_context['get_iva_condition'] = cls.get_iva_condition
+        report_context['get_party_tax_identifier'] = cls.get_party_tax_identifier
+        report_context['get_amount'] = cls.get_amount
         report_context['get_account'] = cls.get_account
         report_context['get_iibb'] = cls.get_iibb
         report_context['get_iibb_name'] = cls.get_iibb_name
         report_context['taxes'] = taxes
         report_context['alicuotas'] = alicuotas
         report_context['iva_conditions'] = iva_conditions
+        report_context['totales_amount'] = totales_amount
+        report_context['totales_untaxed_amount'] = totales_untaxed_amount
+        report_context['totales_iva105'] = totales_iva105
+        report_context['totales_iva21'] = totales_iva21
+        report_context['totales_iva27'] = totales_iva27
         report_context['get_sum_neto_by_tax'] = cls.get_sum_neto_by_tax
         report_context['get_sum_percibido_by_tax'] = \
             cls.get_sum_percibido_by_tax
@@ -338,10 +359,6 @@ class SubdiarioSaleReport(Report, Subdiario):
         report_context['get_sum_percibido_by_tax_and_iva_condition'] = \
             cls.get_sum_percibido_by_tax_and_iva_condition
         return report_context
-
-    @classmethod
-    def format_tipo_comprobante(cls, tipo_cpte):
-        return '%s - %s' % (tipo_cpte.invoice_type, tipo_cpte.rec_name)
 
 
 class SubdiarioSaleType(Wizard):
@@ -359,7 +376,7 @@ class SubdiarioSaleType(Wizard):
             'company': self.start.company.id,
             'from_date': self.start.from_date,
             'to_date': self.start.to_date,
-            'pos': self.start.pos.id,
+            'pos': [p.id for p in self.start.pos],
             }
         return action, data
 
@@ -383,37 +400,33 @@ class SubdiarioSaleTypeReport(Report, Subdiario):
             ('move.date', '>=', data['from_date']),
             ('move.date', '<=', data['to_date']),
             ('company', '=', data['company']),
-            ('pos', '=', data['pos']),
-        ], order=[('invoice_date', 'ASC')])
+            ('pos', 'in', data['pos']),
+            ], order=[
+            ('pos', 'ASC'),
+            ('invoice_date', 'ASC'),
+            ('invoice_type', 'ASC'),
+            ('number', 'ASC'),
+            ])
 
         company = Company(data['company'])
-        pos = Pos(data['pos'])
         pos_sequences = PosSequence.search([
-            ('pos', '=', pos.id)
+            ('pos', 'in', data['pos'])
         ])
-
-        for invoice in invoices:
-            total_amount = invoice.total_amount + total_amount
 
         report_context = super(SubdiarioSaleTypeReport,
             cls).get_context(records, data)
         report_context['company'] = company
         report_context['from_date'] = data['from_date']
         report_context['to_date'] = data['to_date']
-        report_context['pos'] = pos
         report_context['tipos_cptes'] = pos_sequences
         report_context['invoices'] = invoices
-        report_context['total_amount'] = total_amount
-        report_context['format_tipo_comprobante'] = cls.format_tipo_comprobante
         report_context['get_iva'] = cls.get_iva
-        report_context['get_account'] = cls.get_account
         report_context['get_iibb'] = cls.get_iibb
-        report_context['get_iibb_name'] = cls.get_iibb_name
+        report_context['get_iva_condition'] = cls.get_iva_condition
+        report_context['get_party_tax_identifier'] = cls.get_party_tax_identifier
+        report_context['get_amount'] = cls.get_amount
+        report_context['format_ci'] = cls.format_ci
         return report_context
-
-    @classmethod
-    def format_tipo_comprobante(cls, tipo_cpte):
-        return '%s - %s' % (tipo_cpte.invoice_type, tipo_cpte.rec_name)
 
 
 class SubdiarioSaleSubdivision(Wizard):
@@ -431,7 +444,7 @@ class SubdiarioSaleSubdivision(Wizard):
             'company': self.start.company.id,
             'from_date': self.start.from_date,
             'to_date': self.start.to_date,
-            'pos': self.start.pos.id,
+            'pos': [p.id for p in self.start.pos],
             }
         return action, data
 
@@ -454,35 +467,31 @@ class SubdiarioSaleSubdivisionReport(Report, Subdiario):
             ('move.date', '>=', data['from_date']),
             ('move.date', '<=', data['to_date']),
             ('company', '=', data['company']),
-            ('pos', '=', data['pos']),
-        ], order=[('invoice_date', 'ASC')])
+            ('pos', 'in', data['pos']),
+            ], order=[
+            ('pos', 'ASC'),
+            ('invoice_date', 'ASC'),
+            ('invoice_type', 'ASC'),
+            ('number', 'ASC'),
+            ])
 
         company = Company(data['company'])
-        pos = Pos(data['pos'])
         # search subdivisions of Argentina
         subdivisions = Subdivision.search([
             ('country.code', '=', 'AR')
         ], order=[('name', 'ASC')])
-
-        for invoice in invoices:
-            total_amount = invoice.total_amount + total_amount
 
         report_context = super(SubdiarioSaleSubdivisionReport,
             cls).get_context(records, data)
         report_context['company'] = company
         report_context['from_date'] = data['from_date']
         report_context['to_date'] = data['to_date']
-        report_context['pos'] = pos
         report_context['subdivisions'] = subdivisions
         report_context['invoices'] = invoices
-        report_context['total_amount'] = total_amount
-        report_context['format_tipo_comprobante'] = cls.format_tipo_comprobante
         report_context['get_iva'] = cls.get_iva
-        report_context['get_account'] = cls.get_account
         report_context['get_iibb'] = cls.get_iibb
-        report_context['get_iibb_name'] = cls.get_iibb_name
+        report_context['get_iva_condition'] = cls.get_iva_condition
+        report_context['get_party_tax_identifier'] = cls.get_party_tax_identifier
+        report_context['get_amount'] = cls.get_amount
+        report_context['format_ci'] = cls.format_ci
         return report_context
-
-    @classmethod
-    def format_tipo_comprobante(cls, tipo_cpte):
-        return '%s - %s' % (tipo_cpte.invoice_type, tipo_cpte.rec_name)
