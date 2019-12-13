425060a2c31bcb11612eb859af68e7c8c8bb4c7f
Luciano Rossi <lukio@gcoop.coop>
2019-12-13 19:00:43 -0300

    fixes #7 Facturacion en moneda secundaria. Mejora salida del reporte de venta.
    Utiliza los campos currency_rate, party_tax_identifier y party_iva_condition de
    account_invoice_ar.
    
    De esa manera, logramos que el subdiario obtenga los datos del
    comprobante de la factura.

--- subdiario.py
+++ subdiario.py
@@ -2,40 +2,104 @@
 # This file is part of subdiario module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
+import stdnum.ar.cuit as cuit
+import stdnum.ar.dni as dni
+from pysimplesoap.client import SimpleXMLElement
+from unidecode import unidecode
 from decimal import Decimal
-from trytond.pool import Pool
 
+from trytond.pool import Pool
+from trytond.transaction import Transaction
 
 class Subdiario(object):
 
     @classmethod
-    def get_iva(cls, invoice, rate, group_tax='IVA'):
+    def format_ci(cls, iva_condition):
+        iva_condition = iva_condition.lower()
+        if iva_condition in ('exento', 'monotributo'):
+            return iva_condition.upper()[:2]
+        else:
+            if '_' in iva_condition:
+                first, second = iva_condition.split('_')
+            else:
+                first, second = iva_condition.split(' ')
+            return  '%s%s' % (first[:1].upper(), second[:1].upper())
+
+    @classmethod
+    def get_amount(cls, invoice, field):
         Currency = Pool().get('currency.currency')
+        value = getattr(invoice, field)
+        amount = value
+        if invoice.currency != invoice.company.currency:
+            amount = cls.get_secondary_amount(invoice, value)
+        return amount
+
+    @classmethod
+    def get_secondary_amount(cls, invoice, value):
+        Currency = Pool().get('currency.currency')
+        if invoice.pos and invoice.pos.pos_type == 'electronic':
+            afip_tr, = [tr for tr in invoice.transactions
+                if tr.pyafipws_result=='A']
+            request = SimpleXMLElement(unidecode(afip_tr.pyafipws_xml_request))
+            if invoice.pos.pyafipws_electronic_invoice_service == 'wsfex':
+                ctz = Decimal(str(request('Moneda_ctz')))
+            elif invoice.pos.pyafipws_electronic_invoice_service == 'wsfe':
+                ctz = Decimal(str(request('MonCotiz')))
+        currency_rate = invoice.currency_rate or ctz
+        context = dict(date=invoice.currency_date)
+        if currency_rate:
+            context['currency_rate'] = currency_rate
+        with Transaction().set_context(context):
+            amount = Currency.compute(invoice.currency, value,
+                invoice.company.currency)
+        return amount
+
+    @classmethod
+    def get_iva_condition(cls, invoice):
+        return (cls.format_ci(invoice.party_iva_condition
+                or invoice.party.iva_condition))
+
+    @classmethod
+    def get_party_tax_identifier(cls, invoice):
+        code = ''
+        if invoice.party_tax_identifier:
+            code = invoice.party_tax_identifier.code
+        elif invoice.party.vat_number:
+            code = invoice.party.vat_number
+        else:
+            for identifier in invoice.party.identifiers:
+                if identifier.type == 'ar_dni':
+                    code = identifier.code
+
+        if cuit.is_valid(code):
+            code = cuit.format(code)
+        elif dni.is_valid(code):
+            code = dni.format(code)
+        return code
+
+    @classmethod
+    def get_iva(cls, invoice, rate, group_tax='IVA'):
         amount = Decimal('0')
         for invoice_tax in invoice.taxes:
             if (invoice_tax.tax.rate and invoice_tax.tax.rate == Decimal(rate)
                     and invoice_tax.tax.group
                     and group_tax.lower() in invoice_tax.tax.group.code.lower()):
                 tax_amount = invoice_tax.amount
-                if invoice.currency.id != invoice.company.currency.id:
-                    amount += Currency.compute(
-                        invoice.currency, tax_amount, invoice.company.currency)
+                if invoice.currency != invoice.company.currency:
+                    amount += cls.get_secondary_amount(invoice, tax_amount)
                 else:
                     amount += invoice.currency.round(tax_amount)
         return amount
 
     @classmethod
     def get_iibb(cls, invoice):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice_tax in invoice.taxes:
             if (invoice_tax.tax.group and 'iibb' in
                     invoice_tax.tax.group.code.lower()):
                 tax_amount = invoice_tax.amount
-                if invoice.currency.id != invoice.company.currency.id:
-                    amount += Currency.compute(
-                        invoice.currency, tax_amount, invoice.company.currency)
+                if invoice.currency != invoice.company.currency:
+                    amount += cls.get_secondary_amount(invoice, tax_amount)
                 else:
                     amount += invoice.currency.round(tax_amount)
         return amount
@@ -56,140 +120,139 @@ class Subdiario(object):
 
     @classmethod
     def get_other_taxes(cls, invoice):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice_tax in invoice.taxes:
             if (invoice_tax.tax.group and invoice_tax.tax.group.code.lower()
                     not in ['iibb', 'iva']):
                 tax_amount = invoice_tax.amount
-                if invoice.currency.id != invoice.company.currency.id:
-                    amount += Currency.compute(
-                        invoice.currency, tax_amount, invoice.company.currency)
+                if invoice.currency != invoice.company.currency:
+                    amount += cls.get_secondary_amount(invoice, tax_amount)
                 else:
                     amount += invoice.currency.round(tax_amount)
         return amount
 
     @classmethod
     def get_sum_neto_by_tax(cls, tax, invoices):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice in invoices:
             for invoice_tax in invoice.taxes:
                 if invoice_tax.tax == tax:
                     untaxed_amount = invoice_tax.base
-                    if invoice.currency.id != invoice.company.currency.id:
-                        amount += Currency.compute(
-                            invoice.currency, untaxed_amount, invoice.company.currency)
+                    if invoice.currency != invoice.company.currency:
+                        amount += cls.get_secondary_amount(invoice, untaxed_amount)
                     else:
                         amount += invoice.currency.round(untaxed_amount)
         return amount
 
     @classmethod
     def get_sum_percibido_by_tax(cls, tax, invoices):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice in invoices:
             for invoice_tax in invoice.taxes:
                 if invoice_tax.tax == tax:
                     tax_amount = invoice_tax.amount
-                    if invoice.currency.id != invoice.company.currency.id:
-                        amount += Currency.compute(
-                            invoice.currency, tax_amount, invoice.company.currency)
+                    if invoice.currency != invoice.company.currency:
+                        amount += cls.get_secondary_amount(invoice, tax_amount)
                     else:
                         amount += invoice.currency.round(tax_amount)
         return amount
 
     @classmethod
     def get_sum_neto_by_iva_condition(cls, iva_condition, invoices):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
+
         for invoice in invoices:
-            if invoice.party.iva_condition == iva_condition:
+            party_iva_condition = (invoice.party_iva_condition
+                or invoice.party.iva_condition)
+            if party_iva_condition == iva_condition:
                 untaxed_amount = invoice.untaxed_amount
-                if invoice.currency.id != invoice.company.currency.id:
-                    amount += Currency.compute(
-                        invoice.currency, untaxed_amount, invoice.company.currency)
+                if invoice.currency != invoice.company.currency:
+                    amount += cls.get_secondary_amount(invoice, untaxed_amount)
                 else:
                     amount += invoice.currency.round(untaxed_amount)
         return amount
 
     @classmethod
     def get_sum_percibido_by_iva_condition(cls, iva_condition, invoices):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice in invoices:
-            if invoice.party.iva_condition == iva_condition:
+            party_iva_condition = (invoice.party_iva_condition
+                or invoice.party.iva_condition)
+            if party_iva_condition == iva_condition:
                 for invoice_tax in invoice.taxes:
                     if (invoice_tax.tax.group and 'iva' in
                             invoice_tax.tax.group.code.lower()):
                         tax_amount = invoice_tax.amount
-                        if invoice.currency.id != invoice.company.currency.id:
-                            amount += Currency.compute(
-                                invoice.currency, tax_amount, invoice.company.currency)
+                        if invoice.currency != invoice.company.currency:
+                            amount += cls.get_secondary_amount(invoice,
+                                tax_amount)
                         else:
                             amount += invoice.currency.round(tax_amount)
         return amount
 
     @classmethod
-    def get_sum_neto_by_tax_and_iva_condition(cls, tax, iva_condition, invoices):
-        Currency = Pool().get('currency.currency')
+    def get_sum_neto_by_tax_and_iva_condition(cls, tax, iva_condition,
+            invoices):
         amount = Decimal('0')
         for invoice in invoices:
-            if invoice.party.iva_condition == iva_condition:
+            party_iva_condition = (invoice.party_iva_condition
+                or invoice.party.iva_condition)
+            if party_iva_condition == iva_condition:
                 for invoice_tax in invoice.taxes:
                     if invoice_tax.tax == tax:
                         untaxed_amount = invoice.untaxed_amount
-                        if invoice.currency.id != invoice.company.currency.id:
-                            amount += Currency.compute(
-                                invoice.currency, untaxed_amount, invoice.company.currency)
+                        if invoice.currency != invoice.company.currency:
+                            amount += cls.get_secondary_amount(invoice,
+                                untaxed_amount)
                         else:
                             amount += invoice.currency.round(untaxed_amount)
         return amount
 
     @classmethod
-    def get_sum_percibido_by_tax_and_iva_condition(cls, tax, iva_condition, invoices):
-        Currency = Pool().get('currency.currency')
+    def get_sum_percibido_by_tax_and_iva_condition(cls, tax, iva_condition,
+            invoices):
         amount = Decimal('0')
         for invoice in invoices:
-            if invoice.party.iva_condition == iva_condition:
+            party_iva_condition = (invoice.party_iva_condition
+                or invoice.party.iva_condition)
+            if party_iva_condition == iva_condition:
                 for invoice_tax in invoice.taxes:
                     if invoice_tax.tax == tax:
                         tax_amount = invoice_tax.amount
-                        if invoice.currency.id != invoice.company.currency.id:
-                            amount += Currency.compute(
-                                invoice.currency, tax_amount, invoice.company.currency)
+                        if invoice.currency != invoice.company.currency:
+                            amount += cls.get_secondary_amount(invoice,
+                                tax_amount)
                         else:
                             amount += invoice.currency.round(tax_amount)
         return amount
 
     @classmethod
     def get_sum_neto_by_tax_and_subdivision(cls, tax, subdivision, invoices):
-        Currency = Pool().get('currency.currency')
         amount = Decimal('0')
         for invoice in invoices:
             if invoice.invoice_address.subdivision == subdivision:
                 for invoice_tax in invoice.taxes:
                     if invoice_tax.tax == tax:
                         untaxed_amount = invoice.untaxed_amount
-                        if invoice.currency.id != invoice.company.currency.id:
-                            amount += Currency.compute(
-                                invoice.currency, untaxed_amount, invoice.company.currency)
+                        if invoice.currency != invoice.company.currency:
+                            amount += cls.get_secondary_amount(invoice,
+                                untaxed_amount)
                         else:
                             amount += invoice.currency.round(untaxed_amount)
         return amount
 
     @classmethod
-    def get_sum_percibido_by_tax_and_subdivision(cls, tax, subdivision, invoices):
-        Currency = Pool().get('currency.currency')
+    def get_sum_percibido_by_tax_and_subdivision(cls, tax, subdivision,
+            invoices):
         amount = Decimal('0')
         for invoice in invoices:
             if invoice.invoice_address.subdivision == subdivision:
                 for invoice_tax in invoice.taxes:
                     if invoice_tax.tax == tax:
                         tax_amount = invoice_tax.amount
-                        if invoice.currency.id != invoice.company.currency.id:
-                            amount += Currency.compute(
-                                invoice.currency, tax_amount, invoice.company.currency)
+                        if invoice.currency != invoice.company.currency:
+                            amount += cls.get_secondary_amount(invoice,
+                                tax_amount)
                         else:
                             amount += invoice.currency.round(tax_amount)
         return amount
